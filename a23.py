00d5901a5bb6351968e4e112faca2ab4038b8771
kirtenbird <110917933+kirtenbird@users.noreply.github.com>
2023-10-29 15:21:07 +0800

    Create a23.py

---
+++ a23.py
@@ -0,0 +1,16 @@
+import turtle as t
+t.speed(2)
+t.color("red")
+for i in range(4):
+    
+    t.forward(200)
+    t.right(90)
+t.right(45)
+t.penup()
+t.forward(70)
+t.right(-45)
+t.pendown()
+for j in range(4):
+    t.pendown
+    t.forward(100)
+    t.right(90)
